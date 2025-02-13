bb9a9728c8a26ebd304fb437fea50409d3679edc
Maxim Mashnin <onemillerlol@yandex.ru>
2025-02-13 17:23:36 +0500

    Добавил бинарный поиск в алгоритм шифрования

--- src/main/java/com/javarush/mashnin/cryptoanalyzer/service/CaesarCipher.java
+++ src/main/java/com/javarush/mashnin/cryptoanalyzer/service/CaesarCipher.java
@@ -1,17 +1,21 @@
 package com.javarush.mashnin.cryptoanalyzer.service;
 
 import com.javarush.mashnin.cryptoanalyzer.exception.NotValidKeyException;
+
+import java.util.Arrays;
+
 public class CaesarCipher {
     private final char[] alphabet;
     public CaesarCipher(char[] alphabet) {
         this.alphabet = alphabet;
+        Arrays.sort(this.alphabet);
     }
     public String applyCipher(String text, int key) {
         if(key <= -alphabet.length || key >= alphabet.length || key == 0)
             throw new NotValidKeyException(key);
         StringBuilder stringBuilder = new StringBuilder();
         for(char symbol: text.toCharArray()) {
-            int pos = indexOf(alphabet, symbol);
+            int pos = Arrays.binarySearch(alphabet, symbol);
             if(pos >= 0) {
                 int newIndex = (pos + key + alphabet.length) % alphabet.length;
                 stringBuilder.append(alphabet[newIndex]);
@@ -20,12 +24,5 @@ public class CaesarCipher {
                 stringBuilder.append(symbol);
         }
         return stringBuilder.toString();
-    }
-    private int indexOf(char[] array, char target) {
-        for(int i = 0; i < array.length; i++) {
-            if (array[i] == target)
-                return i;
-        }
-        return -1;
     }
 }
